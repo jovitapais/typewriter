673fd83e008baba7340178a938e9f007a65a6982
Jovita <jovitaspais@gmail.com>
2021-11-30 22:35:09 +0000

    Animated Sentence

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+
+let start = 0;
+for (const char of sentence) {
+  
+  setTimeout(() => {
+    // print the char here
+    process.stdout.write(char);
+  }, start); // <= 1s delay to make it noticeable. Can dial it down later.
+  start += 50;
+}
+
