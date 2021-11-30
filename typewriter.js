ee02d7d708ffebc5359e7cf2d4812646c4ca2761
Jovita <jovitaspais@gmail.com>
2021-11-30 22:46:16 +0000

    fixed the bug

--- typewriter.js
+++ typewriter.js
@@ -5,8 +5,8 @@ for (const char of sentence) {
   
   setTimeout(() => {
     // print the char here
-    process.stdout.write(char);
+    process.stdout.write(char + "\n");
   }, start); // <= 1s delay to make it noticeable. Can dial it down later.
-  start += 50;
+    start += 50;
 }
 
